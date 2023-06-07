37a9b0868059dc7d3fb0633fc5bf2046a45bd6ef
Andrii Balan <balan.andrey.93@gmail.com>
2023-06-07 22:27:41 +0300

    dev: optimization

---
+++ 404.php
@@ -0,0 +1,14 @@
+<?php 
+    get_header();
+
+?>
+
+<div class="container toys">
+    <h2 class="subtitle">
+        <a href="<?php echo get_home_url(); ?>">Такої сторінки не існує. Повернутись на головну?</a>
+    </h2>
+</div>
+
+<?php 
+    get_footer();
+?>
